069202feb1d903253ab649f78b767211b599b4b2
Thibault-d <thibaultdelfaud@gmail.com>
2020-04-18 14:28:15 +0200

    chrono with start, pause, reset features

--- src/Components/Chrono.js
+++ src/Components/Chrono.js
@@ -2,24 +2,62 @@ import React, { Component } from "react";
 
 class Chrono extends Component {
   state = {
-    seconds: 0,
-    status: "",
+    seconds: null,
+    running: false,
+    paused: null,
+    interval: null,
   };
+
+  timer = () => {
+    this.state.interval = setInterval(() => {
+      this.setState({
+        seconds: this.state.seconds + 1,
+        running: true,
+      });
+    }, 1000);
+  }
 
   start = () => {
-    setInterval(() => {
-
+    if (this.state.running === true){ // if a Chrono is running already
+      alert("Error, please stop/reset current Chrono before starting a new one!")
+    } else if(this.state.running === false && this.state.paused > 0){ // if Chrono was paused, take the value before the pause
       this.setState({
-        seconds: this.state.seconds +1
+        seconds: this.state.paused
       })
-    }, 1000);
+      this.timer()
+    } else { // if Chrono isn't running and wasn't paused before
+      this.timer()
+    }
+
   };
+
+  stop = () => {
+    this.setState({
+        running: false,
+        paused: this.state.seconds,
+      }, () => {
+        clearInterval(this.state.interval);
+      }
+    );
+  };
+
+  reset = () => {
+    this.setState({
+      running: false,
+      seconds: null,
+      paused: null
+    })
+  }
+
+
 
   render() {
     return (
       <div>
         <div>Number of seconds elapsed: {this.state.seconds}</div>
         <button onClick={this.start}>Start</button>
+        <button onClick={this.stop}>Pause</button>
+        <button onClick={this.reset}>Reset</button>
       </div>
     );
   }
